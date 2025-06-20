e76a017d75db9f146a0184d6b9a6b5726e720654
lucasfelixgit <felixworkspace7@gmail.com>
2025-06-20 20:08:03 -0300

    first commit

---
+++ src/main/java/dev/java10x/NinjaSignUp/NinjaSignUpApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.NinjaSignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaSignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/NinjaSignUp/NinjaSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.NinjaSignUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

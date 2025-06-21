0918914306fdb60fa870c7223d9e8ceb7d874d75
lucasfelixgit <felixworkspace7@gmail.com>
2025-06-20 21:29:57 -0300

    controller created

---
+++ src/main/java/dev/java10x/NinjaSignUp/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.NinjaSignUp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/greetings")
+    public String greetings() {
+        return "This is my first message in this route!";
+    }
+
+}
